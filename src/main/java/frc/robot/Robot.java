a0977cc2e2909b1b81e13f2baf385e265ec83f12
BrandonWerbel <brandon@werbel.org>
2020-11-06 15:27:55 -0500

    Implement Control Panel subsystem in java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -13,6 +13,7 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj.I2C;
 import frc.robot.common.ColorSensor;
 
 /**
