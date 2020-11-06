75e1d8b252dd0c357a95b807f5e7ad3e46c0492c
BrandonWerbel <brandon@werbel.org>
2020-11-06 15:27:55 -0500

    Implement Control Panel subsystem in java

--- src/main/java/frc/robot/commands/ControlPanelCommand.java
+++ src/main/java/frc/robot/commands/ControlPanelCommand.java
@@ -1,6 +1,7 @@
 package frc.robot.commands;
 
-import com.revrobotics.ColorMatchResult;
+
+import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj2.command.CommandBase;
@@ -12,23 +13,42 @@ public class ControlPanelCommand extends CommandBase {
 
     ControlPanelSubsystem controlPanel;
 
-    public ControlPanelCommand() {
+    public ControlPanelCommand(){
         controlPanel = new ControlPanelSubsystem();
     }
 
 
     @Override
-    public void execute() {
+    public void execute(){
 
-        if(controlPanel.getDS().getGameSpecificMessage().length() > 0 && controlPanel.getFMSColor() != null) {
+        if(controlPanel.ds.getGameSpecificMessage().length() > 0 && controlPanel.fmsColor != null){
             controlPanel.getFMSColor();
         }
 
-        if(controlPanel.getSolenoidState() == DoubleSolenoid.Value.kForward) {
-                Color readColor = controlPanel.getColorSensor().getColor();
-                ColorMatchResult resultColor = controlPanel.getColorMatcher().matchClosestColor(readColor);
-                controlPanel.setDetectedColor(resultColor.color);
+        if(controlPanel.solenoidState == DoubleSolenoid.Value.kForward){
+            try{
+                Color readColor = controlPanel.colorSensor.getColor();
+                com.revrobotics.ColorMatchResult resultColor = controlPanel.colorMatcher.matchClosestColor(readColor);
+
+                if (!resultColor.color.equals(Color.kBlack)){
+
+                    controlPanel.detectedColor = resultColor.color;
+                    double lowestDistance = ControlPanelSubsystem.calculateDistance(controlPanel.detectedColor, controlPanel.colors.get(0));
+                    
+                    //Iterates through controlPanel.colors and sets cotrolPanel.detectedColor to the color it is closet to
+                    for(Color color : controlPanel.colors){
+                        if(ControlPanelSubsystem.calculateDistance(controlPanel.detectedColor, controlPanel.colors.get(0)) < lowestDistance){
+                            lowestDistance = ControlPanelSubsystem.calculateDistance(controlPanel.detectedColor, controlPanel.colors.get(0));
+                            controlPanel.detectedColor = color;
+                        }
+                    }
+                }
+            }
+            catch (Exception e){
+                return;
+            }
         }
-        // controlPanel.getCPMoter().set(VictorSPXControlMode.Velocity, (double)controlPanel.getSpeed());
+        controlPanel.cpMotor.set(VictorSPXControlMode.Velocity, (double)controlPanel.speed);
+        controlPanel.cpSolenoid.set(controlPanel.solenoidState);
     }
 }
--- src/main/java/frc/robot/subsystems/ControlPanelSubsystem.java
+++ src/main/java/frc/robot/subsystems/ControlPanelSubsystem.java
@@ -1,18 +1,13 @@
 package frc.robot.subsystems;
 
-// import java.util.HashMap;
 import java.util.List;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
 
 import com.revrobotics.ColorSensorV3;
 import com.revrobotics.ColorMatch;
 
-// import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.I2C.Port;
@@ -20,53 +15,40 @@ import edu.wpi.first.wpilibj.util.Color;
 
 public class ControlPanelSubsystem extends SubsystemBase {
 
-    // private VictorSPX cpMotor;
-    private DoubleSolenoid cpSolenoid;
-    private ColorSensorV3 colorSensor;
-    private DriverStation ds;
-    private ColorMatch colorMatcher;
+    public VictorSPX cpMotor;
+    public DoubleSolenoid cpSolenoid;
+    public ColorSensorV3 colorSensor;
+    public DriverStation ds;
+    public ColorMatch colorMatcher;
 
-    private Color turnToColor = null;
-    private Color detectedColor = null;
-    private Color fmsColor = null;
-    private Color lastColor = null;
+    Color turnToColor = null;
+    public Color detectedColor = null;
+    public Color fmsColor = null;
+    Color lastColor = null;
 
-    private List<String> colorNames;
-    private List<Color> _colors;
-    
+    List<String> colorNames;
+    public List<Color> colors;
 
-    //private LinkedHashMap<String, Color> colors;
-
-    private int speed = 0;
-    private Value solenoidState = Value.kReverse;
+    //TODO: Both of these were "will_reset_to" variables in python, but I couldn't find a java alternative
+    public int speed = 0;
+    public DoubleSolenoid.Value solenoidState = DoubleSolenoid.Value.kReverse;
 
     public ControlPanelSubsystem(){
-        // cpMotor = new VictorSPX(2);
+        cpMotor = new VictorSPX(2);
         cpSolenoid = new DoubleSolenoid(5, 4);
         colorSensor = new ColorSensorV3(Port.kOnboard);
         ds = DriverStation.getInstance();
         colorMatcher = new ColorMatch();
-
-        // colors = new LinkedHashMap<String, Color>();
-        // colors.put("B", new Color(0.175, 0.469, 0.356));
-        // colors.put("G", new Color(0.188, 0.503, 0.310));
-        // colors.put("R", new Color(0.325, 0.453, 0.221));
-        // colors.put("Y", new Color(0.269, 0.550, 0.181));
 
         colorNames.add("B");
         colorNames.add("G");
         colorNames.add("R");
         colorNames.add("Y");
 
-        _colors.add(new Color(0.175, 0.469, 0.356));
-        _colors.add(new Color(0.188, 0.503, 0.310));
-        _colors.add(new Color(0.325, 0.453, 0.221));
-        _colors.add(new Color(0.269, 0.550, 0.181));
-
-        for(Color color : _colors){
-            colorMatcher.addColorMatch(color);
-        }
-        colorMatcher.addColorMatch(Color.kBlack);
+        colors.add(new Color(0.175, 0.469, 0.356));
+        colors.add(new Color(0.188, 0.503, 0.310));
+        colors.add(new Color(0.325, 0.453, 0.221));
+        colors.add(new Color(0.269, 0.550, 0.181));
     }
 
     public void spin(int speed){
@@ -74,65 +56,24 @@ public class ControlPanelSubsystem extends SubsystemBase {
     }
 
     public void setSolenoid(boolean extend){
-        if (extend){
-            solenoidState = Value.kForward;
-        }
-            
-        else{
-            solenoidState = Value.kReverse;
-        }
+        if (extend)
+            solenoidState = DoubleSolenoid.Value.kForward;
+        else
+        solenoidState = DoubleSolenoid.Value.kReverse;
     }
 
-    public Color getFMSColor(){
-        int fmsColorIndex =  _colors.indexOf(ds.getGameSpecificMessage());
-        fmsColor = _colors.get(fmsColorIndex);
+    public void getFMSColor(){
+        int fmsColorIndex = colorNames.indexOf(ds.getGameSpecificMessage());
+        fmsColor = colors.get(fmsColorIndex);
 
-        turnToColor = _colors.get((fmsColorIndex + 2) % _colors.size());
-
-        return fmsColor;
+        turnToColor = colors.get((fmsColorIndex + 2) % colors.size());
     }
 
-    public DriverStation getDS() {
-        return ds;
-    }
+    public static double calculateDistance(Color color1, Color color2){
+        double redDiff = color1.red - color2.red;
+        double greenDiff = color1.green - color2.green;
+        double blueDiff = color1.blue - color2.blue;
 
-    public ColorSensorV3 getColorSensor() {
-        return colorSensor;
-    }
-
-    public ColorMatch getColorMatcher() {
-        return colorMatcher;
-    }
-
-    public Color getDetectedColor(){
-        return detectedColor;
-    }
-
-    public void setDetectedColor(Color detectedColor) {
-        this.detectedColor = detectedColor;
-    }
-
-    // public VictorSPX getCPMoter(){
-        // return cpMotor;
-    // }
-
-    public DoubleSolenoid getCPSolenoid(){
-        return cpSolenoid;
-    }
-
-    public int getSpeed(){
-        return speed;
-    }
-
-    public void setSpeed(int speed){
-        this.speed = speed;
-    }
-
-    public Value getSolenoidState(){
-        return solenoidState;
-    }
-
-    public List<Color> getColors(){
-        return _colors;
+        return Math.sqrt((redDiff * redDiff + greenDiff * greenDiff + blueDiff * blueDiff) / 2);
     }
 }
