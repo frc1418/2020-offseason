247496caa2d95c1542a77b1fe5db79bbd983db10
BrandonWerbel <56608573+BrandonWerbel@users.noreply.github.com>
2020-11-11 09:17:28 -0500

    Drafting branch (#12)
    * Work from programming 11/10/2020 session
    
    Includes updated code for control-panel and everything co-authors were doing as well.
    
    Co-Authored-By: Andrew Lester <23221268+AndrewLester@users.noreply.github.com>
    Co-Authored-By: BobbyMi11er <45018886+BobbyMi11er@users.noreply.github.com>
    Co-Authored-By: Joe Carpenter <32715516+JosephCarpenter@users.noreply.github.com>
    Co-Authored-By: beaster4435 <56608638+beaster4435@users.noreply.github.com>

---
+++ src/main/java/frc/robot/commands/ControlPanelCommand.java
@@ -0,0 +1,35 @@
+package frc.robot.commands;
+
+import com.revrobotics.ColorMatchResult;
+import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj.util.Color;
+
+import frc.robot.subsystems.ControlPanelSubsystem;
+
+public class ControlPanelCommand extends CommandBase {
+
+    ControlPanelSubsystem controlPanel;
+
+    public ControlPanelCommand() {
+        controlPanel = new ControlPanelSubsystem();
+    }
+
+
+    @Override
+    public void execute() {
+
+        if(controlPanel.getDS().getGameSpecificMessage().length() > 0 && controlPanel.getFMSColor() != null) {
+            controlPanel.getFMSColor();
+        }
+
+        if(controlPanel.getSolenoidState() == DoubleSolenoid.Value.kForward) {
+                Color readColor = controlPanel.getColorSensor().getColor();
+                ColorMatchResult resultColor = controlPanel.getColorMatcher().matchClosestColor(readColor);
+                controlPanel.setDetectedColor(resultColor.color);
+        }
+        controlPanel.getCPMoter().set(VictorSPXControlMode.Velocity, (double)controlPanel.getSpeed());
+    }
+}
