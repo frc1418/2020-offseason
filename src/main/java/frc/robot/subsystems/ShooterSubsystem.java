2faa626170bc26e961919970df8eb357d73da82a
AndrewLester <alester220@gmail.com>
2020-11-23 10:00:17 -0500

    Change shooter subsystem motor directions

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -5,28 +5,36 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static frc.robot.Constants.*;
 
 public class ShooterSubsystem extends SubsystemBase {
-    private CANSparkMax shooterMotor1 = new CANSparkMax(SHOOTER_MOTOR_1, MotorType.kBrushless);
-    private CANSparkMax shooterMotor2 = new CANSparkMax(SHOOTER_MOTOR_2, MotorType.kBrushless);
+    private CANSparkMax shooterMotor1 = new CANSparkMax(SHOOTER_MOTOR_1, MotorType.kBrushed);
+    private CANSparkMax shooterMotor2 = new CANSparkMax(SHOOTER_MOTOR_2, MotorType.kBrushed);
+    private Solenoid shooterSolenoid = new Solenoid(0);
     private CANPIDController shooterController = shooterMotor1.getPIDController();
 
     
     public ShooterSubsystem() {
+        shooterMotor1.setInverted(true);
         shooterMotor2.follow(shooterMotor1);
     }
 
     public void activatePiston() {
-        
+        shooterSolenoid.set(true);
+    }
+
+    public void lowerPiston() {
+        shooterSolenoid.set(false);
     }
 
     /**
      * @param shooterSpeed Velocity of shooter motors in RPM
      */
     public void shooter(double shooterSpeed) {
-        shooterController.setReference(shooterSpeed, ControlType.kVelocity);
+        // shooterController.setReference(shooterSpeed, ControlType.kVelocity);
+        shooterMotor1.set(shooterSpeed);
     }
 
 }
