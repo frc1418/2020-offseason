237276b917eb3ed98ca762e8389844a822e67067
AndrewLester <alester220@gmail.com>
2020-12-01 20:33:05 -0500

    Remove lists for mapping color codes to colors

--- src/main/java/frc/robot/subsystems/ControlPanelSubsystem.java
+++ src/main/java/frc/robot/subsystems/ControlPanelSubsystem.java
@@ -2,12 +2,10 @@ package frc.robot.subsystems;
 
 import static frc.robot.Constants.CONTROL_PANEL_MOTOR;
 
-import java.util.List;
 import java.util.logging.Logger;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
-import com.revrobotics.ColorMatch;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
@@ -26,31 +24,6 @@ public class ControlPanelSubsystem extends SubsystemBase {
     private ControlPanelColorSensor colorSensor = new ControlPanelColorSensor(Port.kOnboard);;
     private DriverStation ds = DriverStation.getInstance();
     private ControlPanelColor turnToColor;
-
-    private List<String> colorNames;
-    private List<Color> _colors;
-    
-
-    //private LinkedHashMap<String, Color> colors;
-
-    public ControlPanelSubsystem() {
-        // colors = new LinkedHashMap<String, Color>();
-        // colors.put("B", new Color(0.175, 0.469, 0.356));
-        // colors.put("G", new Color(0.188, 0.503, 0.310));
-        // colors.put("R", new Color(0.325, 0.453, 0.221));
-        // colors.put("Y", new Color(0.269, 0.550, 0.181));
-
-        colorNames.add("B");
-        colorNames.add("G");
-        colorNames.add("R");
-        colorNames.add("Y");
-
-        _colors.add(new Color(0.175, 0.469, 0.356));
-        _colors.add(new Color(0.188, 0.503, 0.310));
-        _colors.add(new Color(0.325, 0.453, 0.221));
-        _colors.add(new Color(0.269, 0.550, 0.181));
-
-    }
 
     public void spin(double speed) {
         cpMotor.set(ControlMode.PercentOutput, speed);
