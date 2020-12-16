e5c242e156e6cbc7419f6e96b28c454e170f08b4
Tim Winters <twinters@wpi.edu>
2020-12-16 16:11:21 -0500

    Control Panel refactor
    Here is a recommendation for refactoring the control panel. Comments in
    a review

--- src/main/java/frc/robot/subsystems/ControlPanelSubsystem.java
+++ src/main/java/frc/robot/subsystems/ControlPanelSubsystem.java
@@ -1,56 +1,67 @@
 package frc.robot.subsystems;
 
-import static frc.robot.Constants.*;
-
-import java.util.logging.Logger;
+import static frc.robot.Constants.CONTROL_PANEL_MOTOR;
+import static frc.robot.Constants.CONTROL_PANEL_SOLENOID_FWD;
+import static frc.robot.Constants.CONTROL_PANEL_SOLENOID_REV;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
-
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.I2C.Port;
-import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.common.ControlPanelColor;
 import frc.robot.common.ControlPanelColorSensor;
-import frc.robot.common.ControlPanelColorSensor.ControlPanelColor;
+import java.util.Arrays;
+import java.util.logging.Logger;
 
 public class ControlPanelSubsystem extends SubsystemBase {
 
-    private Logger logger = Logger.getLogger("ControlPanelSubsystem");
-    private DoubleSolenoid cpSolenoid = new DoubleSolenoid(CONTROL_PANEL_SOLENOID_FWD, CONTROL_PANEL_SOLENOID_REV);
-    private WPI_VictorSPX cpMotor = new WPI_VictorSPX(CONTROL_PANEL_MOTOR);
-    private ControlPanelColorSensor colorSensor = new ControlPanelColorSensor(Port.kOnboard);
-    private DriverStation ds = DriverStation.getInstance();
-    private ControlPanelColor turnToColor;
+  private Logger logger = Logger.getLogger("ControlPanelSubsystem");
+  private DoubleSolenoid cpSolenoid = new DoubleSolenoid(CONTROL_PANEL_SOLENOID_FWD,
+      CONTROL_PANEL_SOLENOID_REV);
+  private WPI_VictorSPX cpMotor = new WPI_VictorSPX(CONTROL_PANEL_MOTOR);
+  private ControlPanelColorSensor colorSensor = new ControlPanelColorSensor(Port.kOnboard,
+      Arrays.asList(ControlPanelColor.getValidColors()));
+  private DriverStation ds = DriverStation.getInstance();
+  private ControlPanelColor turnToColor;
 
-    public void spin(double speed) {
-        cpMotor.set(ControlMode.PercentOutput, speed);
+  public void spin(double speed) {
+    cpMotor.set(ControlMode.PercentOutput, speed);
+  }
+
+  public void setSolenoid(boolean extend) {
+    if (extend) {
+      cpSolenoid.set(Value.kForward);
+    } else {
+      cpSolenoid.set(Value.kReverse);
     }
+    //        cpSolenoid.set(extend ? Value.kForward : Value.kReverse);
+  }
 
-    public void setSolenoid(boolean extend) {
-        if (extend) {
-            cpSolenoid.set(Value.kForward);
-        } else{
-            cpSolenoid.set(Value.kReverse);
-        }
+  public ControlPanelColor getDetectedColor() {
+    return colorSensor.getDetectedColor();
+  }
+
+  public ControlPanelColor getTurnToColor() {
+    return turnToColor;
+  }
+
+  private ControlPanelColor calculateTurnToColor() {
+    ControlPanelColor dsColor = ControlPanelColor.valueOf(ds.getGameSpecificMessage());
+    int totalColors = ControlPanelColor.getValidColors().length;
+    return ControlPanelColor.getValidColors()[(dsColor.ordinal() + 2) % totalColors];
+  }
+
+  @Override
+  public void periodic() {
+    if (ds.getGameSpecificMessage()
+        .isEmpty()) {
+      return;
+      a
+    } if (this.turnToColor == ControlPanelColor.UNKNOWN) {
+      this.turnToColor = calculateTurnToColor();
     }
-
-    public ControlPanelColor getControlPanelColor() {
-        return ControlPanelColor.getColorFromMatch(colorSensor.getMatchedColor());
-    }
-
-    public ControlPanelColor getTurnToColor() {
-        return turnToColor;
-    }
-
-    @Override
-    public void periodic() {
-        if (ds.getGameSpecificMessage().isEmpty()) return;
-
-        // TODO: Once getColorFromCode throws an IllegalArgumentException, we should try catch it here
-        turnToColor = ControlPanelColor.getColorFromCode(ds.getGameSpecificMessage()).getTurnToColor();
-    }
-
+  }
 }
