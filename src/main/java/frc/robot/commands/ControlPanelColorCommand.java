ef319ec42aa57fe77adc557881f20342b3a59adf
Tim Winters <twinters@wpi.edu>
2020-12-16 16:11:21 -0500

    Control Panel refactor
    Here is a recommendation for refactoring the control panel. Comments in
    a review

--- src/main/java/frc/robot/commands/ControlPanelColorCommand.java
+++ src/main/java/frc/robot/commands/ControlPanelColorCommand.java
@@ -1,41 +1,39 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.common.ControlPanelColorSensor.ControlPanelColor;
 import frc.robot.subsystems.ControlPanelSubsystem;
 
 public class ControlPanelColorCommand extends CommandBase {
-    private ControlPanelSubsystem controlPanel;
-    private ControlPanelColor turnToColor;
+  private ControlPanelSubsystem controlPanel;
+  private float direction;
 
-    public ControlPanelColorCommand(ControlPanelColor turnToColor) {
+    public ControlPanelColorCommand() {
         // TODO: Why won't this line do what we want? Let's think about it
         controlPanel = new ControlPanelSubsystem();
-        this.turnToColor = turnToColor;
     }
 
-    @Override
-    public void initialize() {
-        // Extend the solenoid if it isn't already
-        controlPanel.setSolenoid(true);
-    }
+  @Override
+  public void initialize() {
+    controlPanel.setSolenoid(true);
+    direction = Math.signum(controlPanel.getTurnToColor()
+        .ordinal() - controlPanel.getDetectedColor()
+        .ordinal());
+  }
 
-    @Override
-    public void end(boolean interrupted) {
-        controlPanel.setSolenoid(false);
-        controlPanel.spin(0);
-    }
+  @Override
+  public void end(boolean interrupted) {
+    controlPanel.setSolenoid(false);
+    controlPanel.spin(0);
+  }
 
-    @Override
-    public boolean isFinished() {
-        // TODO: Implement equals for ControlPanelColor and use it here
-        return controlPanel.getControlPanelColor().getColor().equals(turnToColor.getColor());
-    }
+  @Override
+  public boolean isFinished() {
+    return controlPanel.getDetectedColor()
+        .equals(controlPanel.getTurnToColor());
+  }
 
-    @Override
-    public void execute() {
-        double direction = Math.signum(turnToColor.ordinal() - controlPanel.getControlPanelColor().ordinal());
-        controlPanel.spin(0.5 * direction);
-    }
+  @Override
+  public void execute() {
+    controlPanel.spin(0.5 * direction);
+  }
 }
